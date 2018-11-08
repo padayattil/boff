a62a39e134a551519449bc8b66f7440132f07ea9
Antony Padayattil <antonywired@gmail.com>
2018-11-08 13:44:54 +0000

    infinite scroll basics

--- src/index.js
+++ src/index.js
@@ -10,6 +10,9 @@ import './images/car-2.jpg';
 import './index.css';
 
 $(document).ready(function () {
+    console.log($( ".infinite-scroller-container:nth-child(1)" ));
+    // let scroller = $( ".infinite-scroller-container:nth-child(1)" );
+    // scroller.clone().after(".infinite-scroller-container:nth-child(1)");
     $('.sidebar-toggler').on('click', () => {
         $('#sidebar').toggleClass(function() {
           if($(this).hasClass('show')) {
